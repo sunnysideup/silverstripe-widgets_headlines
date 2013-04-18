6c11bfdfb64ac40143c33aae8b8ecbf3e709f1be
romain <romain@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-04-18 01:41:05 +0000

    headlines for ss3
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/widgets_headlines/trunk@4445 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- Headlines.php
+++ Headlines.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- *@author nicolaas [at] sunnysideup.co.nz
+ * @author nicolaas [at] sunnysideup.co.nz
  **/
 class Headlines extends Widget {
 
 	static $db = array(
-		"NumberOfHeadlinesShown" => "Int"
+		'NumberOfHeadlinesShown' => 'Int'
 	);
 
 	static $defaults = array(
-		"NumberOfHeadlinesShown" => 5
+		'NumberOfHeadlinesShown' => 5
 	);
 
-	protected static $boolean_field_used_to_identify_headline = "";
+	protected static $boolean_field_used_to_identify_headline = '';
 		static function set_boolean_field_used_to_identify_headline($v) {self::$boolean_field_used_to_identify_headline = $v;}
 
 	static $title = 'Headlines';
@@ -22,20 +23,17 @@ class Headlines extends Widget {
 	static $description = 'Adds a list of identified headlines';
 
 	function getCMSFields() {
-		return new FieldSet(
-			new NumericField("NumberOfHeadlinesShown","Number of Headlines Shown")
+		return new FieldList(
+			new NumericField('NumberOfHeadlinesShown', 'Number of Headlines Shown')
 		);
 	}
 
 	function Links() {
-		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
-		Requirements::themedCSS("widgets_headlines");
-		$where = null;
+		Requirements::themedCSS('widgets_headlines');
+		$entries = BlogEntry::get()->sort('Date', 'DESC')->limit($this->NumberOfHeadlinesShown);
 		if(self::$boolean_field_used_to_identify_headline) {
-			$where = "{$bt}".self::$boolean_field_used_to_identify_headline."{$bt} = 1";
+			$entries = $entries->filter(self::$boolean_field_used_to_identify_headline, 1);
 		}
-		$dos = DataObject::get("BlogEntry",$where, "{$bt}Date{$bt} DESC", null, "0, ".$this->NumberOfHeadlinesShown);
-		return $dos;
+		return $entries;
 	}
-
 }
